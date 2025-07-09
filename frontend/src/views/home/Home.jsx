6a8ccfc62559406022e0c992defa9e0a5e3579b4
vishal nema <vishalnema23@gmail.com>
2025-07-09 23:19:05 +0530

    Update Home.jsx

--- frontend/src/views/home/Home.jsx
+++ frontend/src/views/home/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
   };
 
   useEffect(() => {
-    axios.get("http://localhost:3000/projects/getAll").then((response) => {
+    axios.get("https://ai-code-reviewer-81km.onrender.com/projects/getAll").then((response) => {
       setProjects(response.data.data);
     });
   }, []);
